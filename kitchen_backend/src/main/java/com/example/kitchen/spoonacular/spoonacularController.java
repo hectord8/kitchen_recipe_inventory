f1ec5b28c2c6682f60313b3532c13510166d2355
hectord8 <hdarrach8@icloud.com>
2026-08-17 15:05:30 +1200

    secuirty updates

---
+++ kitchen_backend/src/main/java/com/example/kitchen/spoonacular/spoonacularController.java
@@ -0,0 +1,11 @@
+package com.example.kitchen.spoonacular;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipes/random")
+public class spoonacularController {
+
+
+}
